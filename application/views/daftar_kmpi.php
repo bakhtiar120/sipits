4e556f5f7fd4c324cb208502d521a0ea18505e8d
Cahya SW <cahyasw2@gmail.com>
2020-07-29 23:21:08 +0700

    js

--- application/views/daftar_kmpi.php
+++ application/views/daftar_kmpi.php
@@ -32,74 +32,88 @@
 
         <p>Identitas Pengusul</p>
         <label for="email"><b>NIP/NIDN</b></label>
-        <input type="text" placeholder="Masukkan NIP/NIDN" name="nomor_induk" id="nomor_induk" onKeyUp="this.value=this.value.replace(/[^0-9]/g,'')" required>
+        <input type="text" class="form-control" placeholder="Masukkan NIP/NIDN" name="nomor_induk" id="nomor_induk" onKeyUp="this.value=this.value.replace(/[^0-9]/g,'')" required>
 
         <label for="email"><b>Nama</b></label>
-        <input type="text" placeholder="Masukkan Nama Lengkap dan gelar (jika ada)" id="nama" name="nama" required>
+        <input type="text" class="form-control" placeholder="Masukkan Nama Lengkap dan gelar (jika ada)" id="nama" name="nama" required>
 
 
         <label for="email"><b>Departemen/Fakultas</b></label>
-        <input type="text" placeholder="Masukkan departemen dan/atau fakultas" name="dept" id="dept" required>
+        <input type="text" class="form-control" placeholder="Masukkan departemen dan/atau fakultas" name="dept" id="dept" required>
 
         <label for="email"><b>Universitas asal</b></label>
-        <input type="text" placeholder="Masukkan Universitas (tidak disingkat)" name="univ" id="univ" required>
+        <input type="text" class="form-control" placeholder="Masukkan Universitas (tidak disingkat)" name="univ" id="univ" required>
 
         <label for="email"><b>Email</b></label>
-        <input type="text" placeholder="Masukkan email (diperbolehkan email instansi)" name="email" id="email" required>
+        <input type="text" class="form-control" placeholder="Masukkan email (diperbolehkan email instansi)" name="email" id="email" required>
+        <span class="error text-danger" id="invalid_email">Email yang anda masukkan invalid</span>
+        <br>
+
 
         <label for="email"><b>No HP</b></label>
-        <input type="text" placeholder="Masukkan No HP yang bisa dihubungi" name="no_hp" id="no_hp" onKeyUp="this.value=this.value.replace(/[^0-9]/g,'')" required>
+        <input type="text" class="form-control" placeholder="Masukkan No HP yang bisa dihubungi" name="no_hp" id="no_hp" onKeyUp="this.value=this.value.replace(/[^0-9]/g,'')" required>
+        <span class="error text-danger" id="invalid_no_hp">Nomor HP maksimal 13 digit</span>
+        <br>
+
 
         <label for="email"><b>Alamat Kantor</b></label>
-        <input type="text" placeholder="Masukkan alamat lengkap kantor" name="alamat_kantor" id="alamat_kantor" required>
+        <input type="text" class="form-control" placeholder="Masukkan alamat lengkap kantor" name="alamat_kantor" id="alamat_kantor" required>
 
         <label for="email"><b>Telepon Kantor</b></label>
-        <input type="text" placeholder="Masukkan telepon kantor" name="telepon_kantor" id="telepon_kantor" onKeyUp="this.value=this.value.replace(/[^0-9]/g,'')" required>
+        <input type="text" class="form-control" placeholder="Masukkan telepon kantor" name="telepon_kantor" id="telepon_kantor" onKeyUp="this.value=this.value.replace(/[^0-9]/g,'')" required>
+        <span class="error text-danger" id="telp_kantor">Telepon Kantor maksimal 13</span>
 
         <hr>
 
         <p>Identitas Pembimbing Dosen ITS</p>
         <label for="email"><b>NIP/NIDN</b></label>
-        <input type="text" placeholder="Masukkan NIP/NIDN" name="nomor_pembimbing" id="nomor_pembimbing" onKeyUp="this.value=this.value.replace(/[^0-9]/g,'')" required>
+        <input type="text" class="form-control" placeholder="Masukkan NIP/NIDN" name="nomor_pembimbing" id="nomor_pembimbing" onKeyUp="this.value=this.value.replace(/[^0-9]/g,'')" required>
 
         <label for="email"><b>Nama</b></label>
-        <input type="text" placeholder="Masukkan Nama Pembimbing" name="nama_pembimbing" id="nama_pembimbing" required>
+        <input type="text" class="form-control" placeholder="Masukkan Nama Pembimbing" name="nama_pembimbing" id="nama_pembimbing" required>
 
 
         <label for="email"><b>Departemen</b></label>
-        <input type="text" placeholder="Masukkan departemen" name="departemen_pembimbing" id="departemen_pembimbing" required>
+        <input type="text" class="form-control" placeholder="Masukkan departemen" name="departemen_pembimbing" id="departemen_pembimbing" required>
 
         <label for="email"><b>Fakultas</b></label>
-        <input type="text" placeholder="Masukkan fakultas" name="fakultas_pembimbing" id="fakultas_pembimbing" required>
+        <input type="text" class="form-control" placeholder="Masukkan fakultas" name="fakultas_pembimbing" id="fakultas_pembimbing" required>
 
         <label for="email"><b>Email</b></label>
-        <input type="text" placeholder="Masukkan email (diperbolehkan email instansi)" name="email_pembimbing" id="email_pembimbing" required>
+        <input type="text" class="form-control" placeholder="Masukkan email (diperbolehkan email instansi)" name="email_pembimbing" id="email_pembimbing" required>
+        <span class="error text-danger" id="invalid_email2">Email yang anda masukkan invalid</span>
+        <br>
 
         <label for="email"><b>No HP</b></label>
-        <input type="text" placeholder="Masukkan No HP yang bisa dihubungi" name="hp_pembimbing" id="hp_pembimbing" onKeyUp="this.value=this.value.replace(/[^0-9]/g,'')" required>
+        <input type="text" class="form-control" placeholder="Masukkan No HP yang bisa dihubungi" name="hp_pembimbing" id="hp_pembimbing" onKeyUp="this.value=this.value.replace(/[^0-9]/g,'')" required>
+        <span class="error text-danger" id="invalid_no_hp2">Nomor HP maksimal 13 digit</span>
+        <br>
 
         <hr>
         <p>Identitas Publikasi</p>
 
         <label for="email"><b>Judul Publikasi</b></label>
-        <input type="text" placeholder="Masukkan judul artikel publikasi" name="judul_publikasi" required>
+        <input type="text" class="form-control" placeholder="Masukkan judul artikel publikasi" name="judul_publikasi" required>
 
         <label for="email"><b>Publisher Tujuan</b></label>
-        <input type="text" placeholder="Masukkan tujuan publisher tempat publikasi" name="publisher" required>
+        <input type="text" class="form-control" placeholder="Masukkan tujuan publisher tempat publikasi" name="publisher" required>
 
         <label for="email"><b>Draft Artikel Publikasi</b></label>
-        <input type="file" placeholder="Unggah file dalam bentuk doc/pdf" name="draft_publikasi" required>
+        <input type="file" placeholder="Unggah file dalam bentuk doc/pdf" name="draft_publikasi" id="draft_publikasi" required>
+        <span class="error text-danger" id="invalid_draft"></span><br>
 
         <label for="email"><b>Luaran Publikasi</b></label>
-        <input type="file" placeholder="Unggah file dalam bentuk doc/pdf" name="luaran_publikasi" required>
+        <input type="file" placeholder="Unggah file dalam bentuk doc/pdf" name="luaran_publikasi" id="luaran_publikasi" required>
+        <span class="error text-danger" id="invalid_luaran"></span><br>
 
         <label for="email"><b>MoU</b></label>
-        <input type="file" placeholder="Unggah file dalam bentuk doc/pdf" name="mou_publikasi" required>
+        <input type="file" placeholder="Unggah file dalam bentuk doc/pdf" name="mou_publikasi" id="mou_publikasi" required>
+        <span class="error text-danger" id="invalid_mou"></span><br>
 
         <hr>
         <p>Dengan mendaftar skema KP ini, berarti anda telah menyetujui <a href="#">syarat dan ketentuan</a>.</p>
 
-        <button type="submit" class="registerbtn">Daftar</button>
+        <button type="submit" id="daftar" class="registerbtn">Daftar</button>
       </div>
 
       <div class="container signin">
@@ -211,8 +225,121 @@
           $('#hp_pembimbing').val(resArr.telepon);
         }
       });
+
+      $('#invalid_email').hide();
+
+      $("#email").change(function() {
+        var email = $('#email').val();
+        if (IsEmail(email) == false) { // not email
+          $('#invalid_email').show();
+          $('#email').focus();
+          $('#daftar').prop("disabled", true);
+        } else {
+          $('#invalid_email').hide();
+          $('#daftar').prop("disabled", false);
+        }
+      });
+
+      $('#invalid_email2').hide();
+
+      $("#email_pembimbing").change(function() {
+        var email = $('#email_pembimbing').val();
+        if (IsEmail(email) == false) { // not email
+          $('#invalid_email2').show();
+          $('#email_pembimbing').focus();
+          $('#daftar').prop("disabled", true);
+        } else {
+          $('#invalid_email2').hide();
+          $('#daftar').prop("disabled", false);
+        }
+      });
+
+      $('#telp_kantor').hide();
+      $("#telp_kantor").change(function() {
+        var hp = $('#telp_kantor').val();
+        if (hp.length >= 13) { // not email
+          $('#telp_kantor').show();
+          $('#no_hp').focus();
+          $('#daftar').prop("disabled", true);
+        } else {
+          $('#telp_kantor').hide();
+          $('#daftar').prop("disabled", false);
+        }
+      });
+
+      $('#invalid_no_hp').hide();
+      $("#no_hp").change(function() {
+        var hp = $('#no_hp').val();
+        if (hp.length >= 13) { // not email
+          $('#invalid_no_hp').show();
+          $('#no_hp').focus();
+          $('#daftar').prop("disabled", true);
+        } else {
+          $('#invalid_no_hp').hide();
+          $('#daftar').prop("disabled", false);
+        }
+      });
+
+      $('#invalid_no_hp2').hide();
+      $("#hp_pembimbing").change(function() {
+        var hp = $('#hp_pembimbing').val();
+        if (hp.length >= 13) { // not email
+          $('#invalid_no_hp2').show();
+          $('#hp_pembimbing').focus();
+          $('#daftar').prop("disabled", true);
+        } else {
+          $('#invalid_no_hp2').hide();
+          $('#daftar').prop("disabled", false);
+        }
+      });
+
+      function IsEmail(email) {
+        console.log("email ", email)
+        var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        if (!regex.test(email)) {
+          return false;
+        } else {
+
+          return true;
+        }
+      }
+
+      $("#daftar").click(function() {
+        var allowedFiles = [".doc", ".docx", ".pdf"];
+        var draft_publikasi = $("#draft_publikasi").val();
+        var luaran_publikasi = $("#luaran_publikasi").val();
+        var mou_publikasi = $("#mou_publikasi").val();
+        var invalid_draft = $("#invalid_draft");
+        var invalid_luaran = $("#invalid_luaran");
+        var invalid_mou = $("#invalid_mou");
+        var regex = new RegExp("([a-zA-Z0-9\s_\\.\-:])+(" + allowedFiles.join('|') + ")$");
+        if (!regex.test(draft_publikasi.toLowerCase())) {
+          invalid_draft.innerHTML = "File harus : <b>" + allowedFiles.join(', ');
+          return false;
+        } else {
+          invalid_draft.innerHTML = "";
+          return true;
+        }
+
+        if (!regex.test(luaran_publikasi.toLowerCase())) {
+          invalid_luaran.innerHTML = "File harus : <b>" + allowedFiles.join(', ');
+          return false;
+        } else {
+          invalid_luaran.innerHTML = "";
+          return true;
+        }
+
+        if (!regex.test(mou_publikasi.toLowerCase())) {
+          invalid_mou.innerHTML = "File harus : <b>" + allowedFiles.join(', ');
+          return false;
+        } else {
+          invalid_mou.innerHTML = "";
+          return true;
+        }
+      });
     });
   </script>
+
 </body>
 
 </html>
